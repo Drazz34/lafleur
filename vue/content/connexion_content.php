186efa848ebd9d739479b9cbe2f9c76e34d65bdc
DrazZ34 <jfvillac@hotmail.com>
2023-03-25 16:26:54 +0100

    Début page connexion

---
+++ vue/content/connexion_content.php
@@ -0,0 +1,19 @@
+<section>
+
+    <h3 class="h3">Connectez-vous</h3>
+
+    <form action="" method="post">
+
+    </form>
+
+</section>
+
+<section>
+
+    <h3 class="h3">Créez votre compte</h3>
+
+    <form action="" method="post">
+        
+    </form>
+
+</section>
